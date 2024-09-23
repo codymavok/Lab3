0d617a0c4cf67e6955f88c0595f7caad0dd8d8ab
David Wang <davidwang2151@gmail.com>
2024-09-23 18:11:08 -0400

    Resolved CheckStyle warnings

--- src/main/java/org/translation/LanguageCodeConverter.java
+++ src/main/java/org/translation/LanguageCodeConverter.java
@@ -37,7 +37,7 @@ public class LanguageCodeConverter {
             // TODO Task: use lines to populate the instance variable
             //           tip: you might find it convenient to create an iterator using lines.iterator()
 
-        // Checkstyle: '}' on next line should be alone on a line.
+            // Checkstyle: '}' on next line should be alone on a line.
         }
         catch (IOException | URISyntaxException ex) {
             throw new RuntimeException(ex);
